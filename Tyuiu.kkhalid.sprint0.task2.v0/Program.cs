6b7dd6ac2fbbfcda8158b1ce90ebb5fd2dbd0115
k khalid <khedayatullah452-lgtm>
2025-09-19 19:30:59 +0500

    Task2

---
+++ Tyuiu.kkhalid.sprint0.task2.v0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//ПодклочеTyuiu.kkhalid.sprint0.task2.libuiu.kkhalid.sprint0.task2.v0.Lib;
+
+
+namespace Tyuiu.kkhalid.sprint0.task2.v0
+
+
+internal class Program
+{
+    public static object Dataservice { get; private set; }
+
+    static void Main(string[] args)
+    {
+        //вызов класса DataService и митода GetMessage
+        //из библиотеки Tyuiu.kkhalid.sprint0.task2.v0.Lib
+        Console.WriteLine(Dataservice.GetMessage("khalid"));
+      Console.ReadKey();
+    }
+}
